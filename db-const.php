5da3f3a0b03fe39aec6998c0731798a127ae9dea
akinunsal <akinunsal@hotmail.com>
2016-02-14 13:52:47 +0200

    const > define

--- db-const.php
+++ db-const.php
@@ -1,7 +1,8 @@
 <?php
     # mysql db constants DB_HOST, DB_USER, DB_PASS, DB_NAME
-    const DB_HOST = 'mysql.1freehosting.com';
-    const DB_USER = 'u498161268_pants';
-    const DB_PASS = 'delgado10';
-    const DB_NAME = 'u498161268_pants';
+    # if doesnt work change define to constant
+    define DB_HOST = 'mysql.1freehosting.com';
+    define DB_USER = 'u498161268_pants';
+    define DB_PASS = 'delgado10';
+    define DB_NAME = 'u498161268_pants';
 ?>
