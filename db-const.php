b1d0931262fa9aa108672b6a3f44c80830555178
akinunsal <akinunsal@hotmail.com>
2017-04-12 11:21:06 +0300

    Fixed dbconst to connect to local test server
    no more free hosting

--- db-const.php
+++ db-const.php
@@ -1,7 +1,7 @@
 <?php
     # mysql db constants DB_HOST, DB_USER, DB_PASS, DB_NAME
-    const DB_HOST = 'mysql.1freehosting.com';
-    const DB_USER = 'u498161268_pants';
-    const DB_PASS = 'delgado10';
-    const DB_NAME = 'u498161268_pants';
+    const DB_HOST = 'localhost';
+    const DB_USER = 'root';
+    const DB_PASS = 'asdf';
+    const DB_NAME = 'root';
 ?>
