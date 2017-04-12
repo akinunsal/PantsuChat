cb8443c60d3de95192c184e9aaf89e19efeb8a69
akinunsal <akinunsal@hotmail.com>
2017-04-12 11:21:27 +0300

    fixed

--- db-const.php
+++ db-const.php
@@ -2,6 +2,6 @@
     # mysql db constants DB_HOST, DB_USER, DB_PASS, DB_NAME
     const DB_HOST = 'localhost';
     const DB_USER = 'root';
-    const DB_PASS = 'asdf';
+    const DB_PASS = 'root';
     const DB_NAME = 'root';
 ?>
